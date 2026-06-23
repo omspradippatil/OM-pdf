e69292488f95832c27d373b3a45f4d9d66021196
om pradip patil <omspradippatil2007@gmail.com>
2026-06-23 21:49:44 +0530

    Sitemap Update

--- fix_sitemap.js
+++ fix_sitemap.js
@@ -2,10 +2,12 @@ import fs from 'node:fs';
 
 const sourcePath = 'public/sitemap_final.xml';
 const canonicalPath = 'public/sitemap.xml';
+const gscPath = 'public/sitemap-gsc.xml';
 
 const xml = fs.readFileSync(sourcePath, 'utf8').replace(/^\uFEFF/, '');
 
 fs.writeFileSync(sourcePath, xml, 'utf8');
 fs.writeFileSync(canonicalPath, xml, 'utf8');
+fs.writeFileSync(gscPath, xml, 'utf8');
 
-console.log(`Wrote ${sourcePath} and ${canonicalPath} without a BOM.`);
+console.log(`Wrote ${sourcePath}, ${canonicalPath}, and ${gscPath} without a BOM.`);
